a58b4a7c8a9bf8fd334471b6097a73be1be1d8fb
toeygueoo <shizhi1970122@126.com>
2021-03-10 21:01:04 +0800

    runner统一处理Web请求日志和Thymeleaf测试

---
+++ runner-api/src/main/java/com/azthink/runner/RunnerApiApplication.java
@@ -0,0 +1,14 @@
+package com.azthink.runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnerApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RunnerApiApplication.class,args);
+    }
+
+}
+
