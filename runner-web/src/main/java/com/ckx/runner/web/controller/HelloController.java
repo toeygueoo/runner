a43e11cda95f6d4e0398dc15a8162c5ba8399a7b
toeygueoo <shizhi1970122@126.com>
2021-03-17 20:54:55 +0800

    runner整体修改1

--- runner-web/src/main/java/com/ckx/runner/web/controller/HelloController.java
+++ runner-web/src/main/java/com/ckx/runner/web/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller(value = "adminHelloController")
+@Controller
 @RequestMapping("/hello")
 public class HelloController {
 
