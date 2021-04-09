b193dba7cd8c04a4b3e6871e16826f87a83e50ff
toeygueoo <shizhi1970122@126.com>
2021-04-09 17:29:13 +0800

    下单界面

--- runner-core/src/main/java/com/ckx/runner/core/service/HelloService.java
+++ runner-core/src/main/java/com/ckx/runner/core/service/HelloService.java
@@ -2,4 +2,6 @@ package com.ckx.runner.core.service;
 
 public interface HelloService {
     public String say(String name);
+    public String say2();
+
 }
--- runner-core/src/main/java/com/ckx/runner/core/service/impl/HelloServiceImpl.java
+++ runner-core/src/main/java/com/ckx/runner/core/service/impl/HelloServiceImpl.java
@@ -4,9 +4,14 @@ import com.ckx.runner.core.service.HelloService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class HelloServiceImpl implements HelloService {
+public class HelloServiceImpl implements HelloService{
     @Override
     public String say(String name) {
         return "Hello, " + name;
+    }
+
+    @Override
+    public String say2(){
+        return null;
     }
 }
